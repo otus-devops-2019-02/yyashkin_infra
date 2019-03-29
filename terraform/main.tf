79d19b9963ba8d980420a21f3d3a053d4f767d50
yyashkin <theyashkins@gmail.com>
2019-03-29 21:03:50 +0300

    fix variables.json.example 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "google_compute_instance" "app" {
 
   metadata {
     # путь до публичного ключа
-    ssh-keys = "appuser:${file(var.public_key_path)}"
+    ssh-keys = "yashkin:${file(var.public_key_path)}"
   }
 
   # определение загрузочного диска
