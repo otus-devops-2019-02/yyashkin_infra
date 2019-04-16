92821dfc3a0bb0ece5a682289e519c46f402af0d
yyashkin <theyashkins@gmail.com>
2019-04-16 10:38:09 +0300

    terraform2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,28 +4,10 @@ variable project {
 
 variable region {
   description = "Region"
-
-  # Значение по умолчанию
   default = "europe-west1"
 }
 
-variable public_key_path {
-  # Описание переменной
-  description = "Path to the public key used for ssh access"
-}
-
-variable disk_image {
-  description = "Disk image"
-}
-
-variable private_key_path {
-  # Описание переменной
-  description = "Path to the private key used for ssh access"
-}
-
-variable zone {
-  description = "zone"
-
-  # Значение по умолчанию
-  default = "europe-west1-b"
+variable storage-bucket {
+  type = "list"
+  description = "Storage bucket"
 }
